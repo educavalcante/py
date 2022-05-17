093216ffa1371393dcc0e60bd4d6ce48ba6ba820
educacalcante <eduardo@cpcbrasil.com>
2022-05-17 20:18:19 -0300

    um py

--- aula6.py
+++ aula6.py
@@ -1,6 +1,11 @@
 class Dog:
-    def __init__(self):
+    def __init__(self, raca, idade, nome=None): #posiciais primeiro nomeados depois
         print(f'Eu sou o sefl: {self}')
+        self.nome = nome #publico
+        self._raca = raca #protegido a classe mae e filhas acessam acessar atraves dos metodos getes e seteres
+        self.__idade = idade #privado somente a classe acessa acessar atraves dos metodos getes e seteres
         
-nick = Dog()
-preta = Dog()
+puppy = Dog(nome='rex', idade=5, raca="pastor alemao")
+boomer = Dog(nome='boomer', raca="pudle", idade=1)
+print(puppy.nome)
+print(boomer.nome)
